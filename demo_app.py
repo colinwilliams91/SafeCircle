39d285106ca468b6f0df864219f05c78425f7ef0
jonathanredplanet <92302670+jonathanredplanet@users.noreply.github.com>
2026-03-10 12:21:46 +0700

    added readme_sdk , and modify demo_app.py but need some changes.

--- demo_app.py
+++ demo_app.py
@@ -1,7 +1,9 @@
 from safecircle_sdk import SafeCircleClient
 
 client = SafeCircleClient()
+
 text = "meet me after school and dont tell your parents"
+
 analysis = client.analyze_text(text)
 print("Analysis:", analysis)
 
@@ -10,5 +12,6 @@ print("Privacy Signal:", signal)
 
 summary = client.guardian_view([signal])
 print("Guardian Summary:", summary)
+
 report = client.anonymous_report(signal)
 print("Anonymous Report:", report)
